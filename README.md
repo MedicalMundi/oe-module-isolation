42908073aeff31d7511d496c88c83926f6c2c826
zero <teclaizerai@gmail.com>
2021-08-21 17:57:00 +0200

    Base setup (composer dependencies and tools).

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# oe-module-isolation
-A library for OpenEMR modules
+```text
+«Quodque ferunt Brotean fecisse cupidine mortis,
+Des tua succensae membra cremanda pyrae»
+
+(Ovidio, Ibis, vv. 519–520 (vv. 517–518 in other editions))
+```
+
+# Brotea
+
+
---
+++ tests/PlaceholderTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Brotea\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Brotea\Placeholder
+ */
+class PlaceholderTest extends TestCase
+{
+    public function test_phpunit_works(): void
+    {
+        self::assertTrue(true);
+    }
+}
