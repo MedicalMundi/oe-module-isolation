a2eb366e8488dc2fc7ef7a35d76a77e6a851686e
zero <teclaizerai@gmail.com>
2021-09-13 10:20:45 +0200

    Added LegacyContainerInterface.

---
+++ src/Contract/Container/LegacyContainerInterface.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of medicalmundi/brotea.
+ * (c) 2019-2021  MedicalMundi <info@medicalmundi@com>
+ * (c) 2019-2021  Zerai Teclai <teclaizerai@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Brotea\Contract\Container;
+
+use Psr\Container\ContainerInterface;
+
+interface LegacyContainerInterface extends ContainerInterface
+{
+}
