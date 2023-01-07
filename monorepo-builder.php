cc37de0a5f73226826a441483dc546461fbb5cab
zero <teclaizerai@gmail.com>
2023-01-07 02:15:05 +0100

    [Tools] added symplify/monorepo-builder + downgrade conf.

---
+++ monorepo-builder.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->workers([
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+    ]);
+};
