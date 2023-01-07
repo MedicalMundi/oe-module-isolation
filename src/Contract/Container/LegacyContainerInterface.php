f30ea2a87a844d6f77e404735def4baa8baf8e71
zero <teclaizerai@gmail.com>
2023-01-07 03:19:33 +0100

    Update docheader src/.

--- src/Contract/Container/LegacyContainerInterface.php
+++ src/Contract/Container/LegacyContainerInterface.php
@@ -2,8 +2,8 @@
 
 /**
  * This file is part of medicalmundi/brotea.
- * (c) 2019-2021  MedicalMundi <info@medicalmundi@com>
- * (c) 2019-2021  Zerai Teclai <teclaizerai@gmail.com>
+ * (c) 2019-2023  MedicalMundi <info@medicalmundi@com>
+ * (c) 2019-2023  Zerai Teclai <teclaizerai@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
